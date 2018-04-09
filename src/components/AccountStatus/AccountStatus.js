3cf4702394a914cdfc4d678316a1da2357409f86
Anton Kats <admin@plenty.life>
2018-04-09 18:23:24 -0400

    able to add a component into Mattermost

---
+++ src/components/AccountStatus/AccountStatus.js
@@ -0,0 +1,14 @@
+import { nSQL } from "nano-sql";
+
+import React, {PureComponent} from 'react';
+
+
+class AccountStatus extends PureComponent {
+    render() {
+        return <div>
+            Account balance
+        </div>
+    }
+}
+
+export default AccountStatus
