c5685a13ea2a8cd43c7bb7528d4de54fda0b7380
Anton Kats <admin@plenty.life>
2018-04-19 15:42:00 -0400

    AFFECTS: integration WORK:    not enough funds modal; visual tests STATUS:  styled TEST:    -

--- src/components/ErrorModals/NotEnoughFunds.js
+++ src/components/ErrorModals/NotEnoughFunds.js
@@ -1,6 +1,9 @@
 // @flow
 import React from 'react'
 import {Modal} from 'react-bootstrap'
+import Balance from '../AccountStatus/Balance'
+import {CRON_TIME} from '../../state/GlobalState'
+import './notEnoughFundsStyle.css'
 
 type Props = {
   show: boolean,
@@ -10,11 +13,18 @@ type Props = {
 }
 
 export function NotEnoughFundsForMessageModal (props: Props) {
-  return <Modal show={props.show} onHide={props.onClose} keyboard={true}>
+  return <Modal dialogClassName={'not-enough-funds-modal'} show={props.show} onHide={props.onClose} keyboard={true}>
     <Modal.Header closeButton={true}>
-      <Modal.Title>Modal title</Modal.Title>
+      <Modal.Title>Not enough funds</Modal.Title>
     </Modal.Header>
 
-    <Modal.Body>One fine body...</Modal.Body>
+    <Modal.Body>
+      <p>It costs <Balance amount={1}/> to send a message</p>
+      <p>Those go into the community pot, which then gets split, based on the ratings your messages (and messages of others)
+      have received</p>
+      <p>
+        The community pot is split every {CRON_TIME} minutes
+      </p>
+    </Modal.Body>
   </Modal>
 }
--- test/visual/visualTests.jsx
+++ test/visual/visualTests.jsx
@@ -3,36 +3,38 @@ import ReactDOM from 'react-dom'
 import {AccountStatus, MessageRating, NotEnoughFundsForMessageModal} from '../../src/index'
 import {nSQL} from 'nano-sql'
 import './visualTests.css'
-import {currentCommunityId, getCurrentAgentId} from '../../src/state/GlobalState'
+import {getCurrentCommunityId, getCurrentAgentId} from '../../src/state/GlobalState'
 import {addCommunitySharePoints, setBalance} from '../../src/db/AgentWalletTable'
 import {addAgentToCommunity} from '../../src/actions/AgentActions'
 import {initializeCommunity} from '../../src/accounting/Accounting'
 import {setCommunityBalance} from '../../src/db/CommunityTable'
 
 nSQL().onConnected(async () => {
+  console.log('Visual tests. Db connected')
+
   const OTHER_AGENT_ID = 'oid'
-  await addAgentToCommunity(getCurrentAgentId(), currentCommunityId)
-  await addAgentToCommunity(OTHER_AGENT_ID, currentCommunityId)
-  const success = await initializeCommunity(currentCommunityId)
+  await addAgentToCommunity(getCurrentAgentId(), getCurrentCommunityId())
+  await addAgentToCommunity(OTHER_AGENT_ID, getCurrentCommunityId())
+  const success = await initializeCommunity(getCurrentCommunityId())
 
   console.log('Initialized community', success)
 
-  await setCommunityBalance(currentCommunityId, 10)
-  // await setBalance(getCurrentAgentId(), currentCommunityId, 20)
-  await setBalance(getCurrentAgentId(), currentCommunityId, -1)
-  await addCommunitySharePoints(getCurrentAgentId(), currentCommunityId, 1)
-  await addCommunitySharePoints(OTHER_AGENT_ID, currentCommunityId, 2)
+  await setCommunityBalance(getCurrentCommunityId(), 10)
+  // await setBalance(getCurrentAgentId(), getCurrentCommunityId(), 20)
+  await setBalance(getCurrentAgentId(), getCurrentCommunityId(), -1)
+  await addCommunitySharePoints(getCurrentAgentId(), getCurrentCommunityId(), 1)
+  await addCommunitySharePoints(OTHER_AGENT_ID, getCurrentCommunityId(), 2)
 
   let b = 1
   setInterval(() => {
-    setBalance(getCurrentAgentId(), currentCommunityId, (b += 1))
-    setCommunityBalance(currentCommunityId, b += 1)
+    setBalance(getCurrentAgentId(), getCurrentCommunityId(), (b += 1))
+    setCommunityBalance(getCurrentCommunityId(), b += 1)
   }, 3000)
 
   function ComponentDisplay () {
     console.log('Test Component Display rendering')
     return <div className={'tests-container'}>
-      <div><AccountStatus agentId={getCurrentAgentId()} communityId={currentCommunityId}/></div>
+      <div><AccountStatus agentId={getCurrentAgentId()} communityId={getCurrentCommunityId()}/></div>
       <div>
         Rating
         <MessageRating/>
