bbd3da944d748bb707795e417f9733693b56180d
anton <antonkats@gmail.com>
2018-05-10 18:31:57 -0400

    credit limit is properly updated in transactions. tested. not yet tested for pot splits

--- test/accounting/transactionTestsPeer.js
+++ test/accounting/transactionTestsPeer.js
@@ -2,7 +2,7 @@ import test from 'ava'
 import {setupDb} from '../utils'
 import {addAgentToCommunity} from '../../src/actions/AgentActions'
 import {makeTransaction} from '../../src/actions/AccountingActions'
-import {AGENT_WALLET_TABLE, getWallet, setBalance} from '../../src/db/AgentWalletTable'
+import {AGENT_WALLET_TABLE, getWallet, setBalance, setCreditLimit} from '../../src/db/AgentWalletTable'
 import {NOT_ENOUGH_FUNDS, RECIPIENT_DOES_NOT_EXIST} from '../../src/utils/UserErrors'
 import {DEFAULT_CREDIT_LIMIT} from '../../src/accounting/AccountingGlobals'
 import {Decimal} from 'decimal.js'
@@ -35,12 +35,14 @@ test.serial('transaction without recipient existing', async t => {
 test.serial('transaction without sender existing', async t => {
   await setupDb(t)
   await addAgentToCommunity(AGENT_RECIPIENT, COMMUNITY_ID)
+  // await setCreditLimit(AGENT_RECIPIENT, COMMUNITY_ID, )
 
   const res = await makeTransaction(AGENT_SENDER, COMMUNITY_ID, 1, AGENT_RECIPIENT, TRANSACTION_TYPE)
   const ws = await getWallet(AGENT_SENDER, COMMUNITY_ID)
   const wr = await getWallet(AGENT_RECIPIENT, COMMUNITY_ID)
   t.is(ws, null)
   t.is(wr.balance, 0)
+  t.is(wr.creditLimit, DEFAULT_CREDIT_LIMIT)
   t.false(res.status)
   t.is(res.value, NOT_ENOUGH_FUNDS)
 })
@@ -55,6 +57,8 @@ test.serial('transaction without sender having enough funds', async t => {
   const wr = await getWallet(AGENT_RECIPIENT, COMMUNITY_ID)
   t.is(ws.balance, 10)
   t.is(wr.balance, 0)
+  t.is(ws.creditLimit, DEFAULT_CREDIT_LIMIT)
+  t.is(wr.creditLimit, DEFAULT_CREDIT_LIMIT)
   t.false(res.status)
   t.is(res.value, NOT_ENOUGH_FUNDS)
 })
@@ -66,30 +70,60 @@ test.serial('first proper transaction', async t => {
   const wr = await getWallet(AGENT_RECIPIENT, COMMUNITY_ID)
   t.is(ws.balance, -0.001)
   t.is(wr.balance, amount)
+  t.is(ws.creditLimit, DEFAULT_CREDIT_LIMIT)
+  t.is(wr.creditLimit, Decimal(1.75).times(10.001).plus(DEFAULT_CREDIT_LIMIT).toNumber())
   t.true(res.status)
   t.falsy(res.value)
 })
 
-test.serial('second transaction with an amount too precise', async t => {
-  const amount = 1.00009 // should get rounded
-  const res = await makeTransaction(AGENT_SENDER, COMMUNITY_ID, amount, AGENT_RECIPIENT, TRANSACTION_TYPE)
-  const ws = await getWallet(AGENT_SENDER, COMMUNITY_ID)
-  const wr = await getWallet(AGENT_RECIPIENT, COMMUNITY_ID)
-  t.is(ws.balance, -1.001)
-  t.is(wr.balance, 11.001)
-  t.true(res.status)
-  t.falsy(res.value)
-})
+var previous = {
+  COMMUNITY_ID: {
+    AGENT_RECIPIENT: {creditLimit: -1, balance: null},
+    AGENT_SENDER: {creditLimit: -1, balance: null}
+  },
+  COMMUNITY_ID_2: {
+    AGENT_RECIPIENT: {creditLimit: -1, balance: null},
+    AGENT_SENDER: {creditLimit: -1, balance: null}
+  }
+}
 
 test.serial('transaction in opposite direction', async t => {
   const amount = 3.00009 // should get rounded
   const res = await makeTransaction(AGENT_RECIPIENT, COMMUNITY_ID, amount, AGENT_SENDER, TRANSACTION_TYPE)
   const ws = await getWallet(AGENT_SENDER, COMMUNITY_ID)
   const wr = await getWallet(AGENT_RECIPIENT, COMMUNITY_ID)
-  t.is(ws.balance, 1.999)
-  t.is(wr.balance, 8.001)
+  const wscl = Decimal(1.75).times(3).plus(DEFAULT_CREDIT_LIMIT).toNumber()
+  const wrcl = Decimal(1.75).times(10.001).plus(DEFAULT_CREDIT_LIMIT).toNumber()
+
+  t.is(ws.balance, 2.999)
+  t.is(wr.balance, 7.001)
+  t.is(ws.creditLimit, wscl)
+  t.is(wr.creditLimit, wrcl)
   t.true(res.status)
   t.falsy(res.value)
+
+  previous.COMMUNITY_ID.AGENT_RECIPIENT.creditLimit = wrcl
+  previous.COMMUNITY_ID.AGENT_SENDER.creditLimit = wscl
+})
+
+test.serial('second transaction with an amount too precise', async t => {
+  const amount = 4.00009 // should get rounded
+  const res = await makeTransaction(AGENT_SENDER, COMMUNITY_ID, amount, AGENT_RECIPIENT, TRANSACTION_TYPE)
+  const ws = await getWallet(AGENT_SENDER, COMMUNITY_ID)
+  const wr = await getWallet(AGENT_RECIPIENT, COMMUNITY_ID)
+  const wscl = previous.COMMUNITY_ID.AGENT_SENDER.creditLimit
+  const wrcl = previous.COMMUNITY_ID.AGENT_RECIPIENT.creditLimit + 4
+
+  t.is(ws.balance, -1.001)
+  t.is(wr.balance, 11.001)
+  t.is(ws.creditLimit, wscl)
+  t.is(wr.creditLimit, wrcl)
+  t.true(res.status)
+  t.falsy(res.value)
+
+  previous.COMMUNITY_ID.AGENT_RECIPIENT.creditLimit = wrcl
+  previous.COMMUNITY_ID.AGENT_RECIPIENT.balance = 11.001
+  previous.COMMUNITY_ID.AGENT_SENDER.balance = -1.001
 })
 
 // with a second community. same thing. improper setups. then proper setup.
@@ -111,10 +145,13 @@ test.serial('transaction without sender existing in second community', async t =
   const wrC1 = await getWallet(AGENT_RECIPIENT, COMMUNITY_ID)
   const wsC2 = await getWallet(AGENT_SENDER, COMMUNITY_ID_2)
   const wrC2 = await getWallet(AGENT_RECIPIENT, COMMUNITY_ID_2)
-  t.is(wsC1.balance, 1.999)
-  t.is(wrC1.balance, 8.001)
+  t.is(wsC1.balance, previous.COMMUNITY_ID.AGENT_SENDER.balance)
+  t.is(wrC1.balance, previous.COMMUNITY_ID.AGENT_RECIPIENT.balance)
+  t.is(wsC1.creditLimit, previous.COMMUNITY_ID.AGENT_SENDER.creditLimit)
+  t.is(wrC1.creditLimit, previous.COMMUNITY_ID.AGENT_RECIPIENT.creditLimit)
   t.is(wsC2, null)
   t.is(wrC2.balance, 0)
+  t.is(wrC2.creditLimit, DEFAULT_CREDIT_LIMIT)
   t.false(res.status)
   t.is(res.value, NOT_ENOUGH_FUNDS)
 })
@@ -132,10 +169,14 @@ test.serial('transaction without sender having enough funds in second community'
   wsC2 = await getWallet(AGENT_SENDER, COMMUNITY_ID_2)
   const wrC2 = await getWallet(AGENT_RECIPIENT, COMMUNITY_ID_2)
 
-  t.is(wsC1.balance, 1.999)
-  t.is(wrC1.balance, 8.001)
+  t.is(wsC1.balance, previous.COMMUNITY_ID.AGENT_SENDER.balance)
+  t.is(wrC1.balance, previous.COMMUNITY_ID.AGENT_RECIPIENT.balance)
+  t.is(wsC1.creditLimit, previous.COMMUNITY_ID.AGENT_SENDER.creditLimit)
+  t.is(wrC1.creditLimit, previous.COMMUNITY_ID.AGENT_RECIPIENT.creditLimit)
   t.is(wsC2.balance, 10)
   t.is(wrC2.balance, 0)
+  t.is(wsC2.creditLimit, DEFAULT_CREDIT_LIMIT)
+  t.is(wrC2.creditLimit, DEFAULT_CREDIT_LIMIT)
   t.false(res.status)
   t.is(res.value, NOT_ENOUGH_FUNDS)
 })
@@ -147,11 +188,39 @@ test.serial('first proper transaction in second community', async t => {
   const wrC1 = await getWallet(AGENT_RECIPIENT, COMMUNITY_ID)
   const wsC2 = await getWallet(AGENT_SENDER, COMMUNITY_ID_2)
   const wrC2 = await getWallet(AGENT_RECIPIENT, COMMUNITY_ID_2)
+  const wrcl = Decimal(1.75).times(10.001).plus(DEFAULT_CREDIT_LIMIT).toNumber()
 
-  t.is(wsC1.balance, 1.999)
-  t.is(wrC1.balance, 8.001)
+  t.is(wsC1.balance, previous.COMMUNITY_ID.AGENT_SENDER.balance)
+  t.is(wrC1.balance, previous.COMMUNITY_ID.AGENT_RECIPIENT.balance)
+  t.is(wsC1.creditLimit, previous.COMMUNITY_ID.AGENT_SENDER.creditLimit)
+  t.is(wrC1.creditLimit, previous.COMMUNITY_ID.AGENT_RECIPIENT.creditLimit)
   t.is(wsC2.balance, -0.001)
   t.is(wrC2.balance, 10.001)
+  t.is(wsC2.creditLimit, DEFAULT_CREDIT_LIMIT)
+  t.is(wrC2.creditLimit, wrcl)
   t.true(res.status)
   t.falsy(res.value)
+})
+
+test.serial('checking credit limits with more members', async t => {
+  await addAgentToCommunity('3', COMMUNITY_ID)
+  await addAgentToCommunity('4', COMMUNITY_ID)
+  await addAgentToCommunity('5', COMMUNITY_ID)
+
+  await makeTransaction(AGENT_SENDER, COMMUNITY_ID, 2, '3', TRANSACTION_TYPE)
+
+  const ws = await getWallet(AGENT_SENDER, COMMUNITY_ID)
+  const wr = await getWallet(AGENT_RECIPIENT, COMMUNITY_ID)
+  const wscl = previous.COMMUNITY_ID.AGENT_SENDER.creditLimit
+  const wrcl = previous.COMMUNITY_ID.AGENT_RECIPIENT.creditLimit
+  t.is(ws.creditLimit, wscl)
+  t.is(wr.creditLimit, wrcl)
+
+  const w3 = await getWallet('3', COMMUNITY_ID)
+  const w4 = await getWallet('4', COMMUNITY_ID)
+  const w5 = await getWallet('5', COMMUNITY_ID)
+  t.is(w4.creditLimit, DEFAULT_CREDIT_LIMIT)
+  t.is(w5.creditLimit, DEFAULT_CREDIT_LIMIT)
+
+  t.is(w3.creditLimit, Decimal(4.96).times(2).plus(DEFAULT_CREDIT_LIMIT).toNumber())
 })
