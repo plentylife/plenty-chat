c59834c8419a842048b34ca5695f0aa4261c2c6f
anton <antonkats@gmail.com>
2018-05-11 14:38:45 -0400

    splitting community pot evenly, not basing on share points since tasks are not implemented yet.

--- src/accounting/CommunityPot.js
+++ src/accounting/CommunityPot.js
@@ -4,6 +4,7 @@ import {getAllCommunitySharePoints} from '../db/AgentWalletTable'
 import {floorWithPrecision} from './utils'
 import './required'
 import {Decimal} from 'decimal.js'
+import {MAX_PRECISION_IN_AGENT_AMOUNTS} from './AccountingGlobals'
 
 export type PotSplitEntry = {
   agentId: string, amount: number
@@ -13,27 +14,43 @@ export async function calculateCommunityPotSplit (communityId: string, community
   Promise<Array<PotSplitEntry>> {
   const points = await getAllCommunitySharePoints(communityId)
 
-  const pointsSum = points.reduce((acc, next) => (acc + next.communitySharePoints), 0)
+  const membersCount = points.length
+  const splitAmount = floorWithPrecision(Decimal(communityBalance).div(membersCount), MAX_PRECISION_IN_AGENT_AMOUNTS)
 
-  let amountsSum = Decimal(0)
-  let largestShare = {agentId: null, share: -1}
   const amounts = points.map(p => {
     const pn = Object.assign({}, p)
-    if (pointsSum !== 0) {
-      pn.amount = floorWithPrecision(Decimal(p.communitySharePoints).div(pointsSum).times(communityBalance))
-    } else {
-      pn.amount = 0
-    }
-    if (pn.amount.gt(largestShare.share)) largestShare = {agentId: p.agentId, share: pn.amount}
-    amountsSum = amountsSum.plus(pn.amount)
-    pn.amount = pn.amount.toNumber()
+    pn.amount = splitAmount.toNumber()
     return pn
   })
 
-  // have to make sure that community balance is fully split, and there are no leftovers that will slowly throw everything off balance
-  const leftOver = Decimal(communityBalance).minus(amountsSum)
-  if (leftOver.lessThan(0)) throw new RangeError('Something is very wrong with community pot split calculations. The sum of all agent balances is higher than community balance')
-  const largest = amounts.find(p => (p.agentId === largestShare.agentId))
-  largest.amount = Decimal(largest.amount).plus(leftOver).toNumber()
   return amounts
 }
+
+// export async function calculateCommunityPotSplit (communityId: string, communityBalance: number):
+//   Promise<Array<PotSplitEntry>> {
+//   const points = await getAllCommunitySharePoints(communityId)
+//
+//   const pointsSum = points.reduce((acc, next) => (acc + next.communitySharePoints), 0)
+//
+//   let amountsSum = Decimal(0)
+//   let largestShare = {agentId: null, share: -1}
+//   const amounts = points.map(p => {
+//     const pn = Object.assign({}, p)
+//     if (pointsSum !== 0) {
+//       pn.amount = floorWithPrecision(Decimal(p.communitySharePoints).div(pointsSum).times(communityBalance))
+//     } else {
+//       pn.amount = 0
+//     }
+//     if (pn.amount.gt(largestShare.share)) largestShare = {agentId: p.agentId, share: pn.amount}
+//     amountsSum = amountsSum.plus(pn.amount)
+//     pn.amount = pn.amount.toNumber()
+//     return pn
+//   })
+//
+//   // have to make sure that community balance is fully split, and there are no leftovers that will slowly throw everything off balance
+//   const leftOver = Decimal(communityBalance).minus(amountsSum)
+//   if (leftOver.lessThan(0)) throw new RangeError('Something is very wrong with community pot split calculations. The sum of all agent balances is higher than community balance')
+//   const largest = amounts.find(p => (p.agentId === largestShare.agentId))
+//   largest.amount = Decimal(largest.amount).plus(leftOver).toNumber()
+//   return amounts
+// }
