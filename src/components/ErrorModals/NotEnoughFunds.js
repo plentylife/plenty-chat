99b1d5568514d93e64699892e25d0caf261528ac
Anton Kats <admin@plenty.life>
2018-05-16 11:45:55 -0400

    changed the message in NotEnoughFunds modal

--- src/components/ErrorModals/NotEnoughFunds.js
+++ src/components/ErrorModals/NotEnoughFunds.js
@@ -4,6 +4,7 @@ import {Modal} from 'react-bootstrap'
 import Balance from '../AccountStatus/Balance'
 import {CRON_TIME} from '../../state/GlobalState'
 import './notEnoughFundsStyle.css'
+import {THANKS} from '../utils'
 
 type Props = {
   show: boolean,
@@ -20,11 +21,12 @@ export function NotEnoughFundsForMessageModal (props: Props) {
 
     <Modal.Body>
       <p>It costs <Balance amount={1}/> to send a message</p>
-      <p>Those go into the community pot, which then gets split, based on the ratings your messages (and messages of others)
-      have received</p>
-      <p>
-        The community pot is split every {CRON_TIME} minutes
-      </p>
+      <p>Other community members might donate you some {THANKS}, but in the meantime you {'won\'t'} be able to message</p>
     </Modal.Body>
   </Modal>
 }
+
+// {/*<p>Those go into the community pot, which then gets split evenly between all members</p>*/}
+// {/*<p>*/}
+// {/*The community pot is split every {CRON_TIME} minutes*/}
+// {/*</p>*/}
