df9bc0c6d16aebba59b3c4d60c7ff9b500f21629
Anton Kats <admin@plenty.life>
2018-05-21 15:19:53 -0400

    tutorial text

--- src/components/Tutorial/index.js
+++ src/components/Tutorial/index.js
@@ -8,22 +8,26 @@ import ScreenCaveat from './ScreenCaveat'
 import IntroJs from 'intro.js/intro'
 import 'intro.js/introjs.css'
 import {getBooleanSetting, pushSimpleSetting} from '../../db/SettingsTable'
+import {TENGE} from '../utils'
 
 export const DASH = '\u2013'
 
-export const ACCOUNT_INTRO = 'this is account panel intro'
+const PlentySpan = '<span class="plenty-word">Plenty</span>'
+const ThanksSpan = `<span class='thanks-word'>${TENGE}hanks</span>\n`
 
-export const BALANCE_INTRO = 'balance intro'
+export const ACCOUNT_INTRO = `This is the heart of ${PlentySpan}. It shows information about your account standing and the community.`
 
-export const DEMURRAGE_INTRO = 'dem intro'
+export const BALANCE_INTRO = `This is your account balance in ${ThanksSpan}`
+export const DEMURRAGE_INTRO = `Since ${ThanksSpan} are a natural currency, they spoil. This is the rate at which your balance is spoiling. The more you spend, the slower your money will spoil.`
 
-export const CREDIT_LIMIT_INTRO = 'credit limit'
+export const CREDIT_LIMIT_INTRO = `${ThanksSpan} are a mutual credit currency, meaning that you can have a negative balance, up to the credit limit. This limit changes based ` +
+  'on how much you earn.'
 
-export const COMMUNITY_BALANCE_INTRO = 'com bal intro'
+export const COMMUNITY_BALANCE_INTRO = `Some ${ThanksSpan}, like the ones that spoiled, go into the community pot. This account is held jointly between all members of the community.`
 
-export const SHARE_INTRO = 'share intro'
+export const SHARE_INTRO = `Every so often, the community pot is split between members. This is your share.`
 
-export const GIVE_BUTTON_INTRO = 'give intro'
+export const GIVE_BUTTON_INTRO = `Whenever you see a message that deserves some ${ThanksSpan}, use this button to give some.`
 
 const MAIN_INTRO_SETTING_KEY = 'main_intro'
 const GIVE_BUTTON_INTRO_SETTING_KEY = 'give_button_intro'
