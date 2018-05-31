559dd6f67082863297a0d16698c01cca1b61289b
Anton Kats <admin@plenty.life>
2018-05-31 12:43:16 -0400

    updated plenty version

--- src/sync/index.js
+++ src/sync/index.js
@@ -14,7 +14,7 @@ const TABLE_SYNC_CHANNEL = 'table-sync'
 const READY_CHANNEL = 'ready'
 
 export const NO_EVENTS_BEFORE = 1524749468216
-const MIN_PLENTY_VERSION = 180528
+const MIN_PLENTY_VERSION = 180530
 
 export type Peer = {
   agentId: string,
