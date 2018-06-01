2cbe42516a5ba3b02470eaef6c82df940c49c9ae
Anton Kats <admin@plenty.life>
2018-06-01 18:34:22 -0400

    demurrage was removing all community share points, instead of doing nothing

--- src/accounting/Demurrage.js
+++ src/accounting/Demurrage.js
@@ -61,6 +61,7 @@ export function calculateDemurrageForAgent (agentWallet: Wallet): DemurrageByPro
   return {
     balance: _cda(agentWallet, 'balance', balanceRate),
     // communitySharePoints: _cda(agentWallet, 'communitySharePoints', balanceRate), fixme. for not community share points are not replenished, so they should not depreciate
+    communitySharePoints: 0,
     incomingStat: _cda(w, 'incomingStat', STATISTICS_DEMURRAGE_RATE),
     outgoingStat: _cda(w, 'outgoingStat', STATISTICS_DEMURRAGE_RATE),
     creditLimit: creditLimit
