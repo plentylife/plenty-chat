78534a57cedb141dc6a31f6f0ba8829c8f797e13
Anton Kats <admin@plenty.life>
2018-06-01 23:40:06 -0400

    updated version

--- src/state/GlobalState.js
+++ src/state/GlobalState.js
@@ -7,7 +7,7 @@ let _currentCommunityId = null
 let _sendTableSync = false
 let _sendEventSync = false
 
-export const PLENTY_VERSION = 180530
+export const PLENTY_VERSION = 180601
 
 export const DB_ID = PLENTY_VERSION.toString()
 
